fa337806b7f7f28d3c19df4a3f628d80135a01e1
165432nandan <162699946+165432nandan@users.noreply.github.com>
2024-10-21 15:35:21 +0000

    basics

---
+++ 01basic.js
@@ -0,0 +1,7 @@
+const regno=200
+let name="nandan"
+var number="2000"
+
+// var is not used because block scope
+console.table([regno,number])
+//to print in a table once
---
+++ 02basic.js
@@ -0,0 +1,16 @@
+//Data types
+
+//boolean -> True/False
+//string  ->"Nandan"
+//bigint 
+//number
+//null -> standalone
+//undefined  -> eg:
+let state;  //eg for undefined
+//symbol ->unique 
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
